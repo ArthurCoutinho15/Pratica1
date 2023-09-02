8fda01e3194d894bb335555beb4415f8abcc6725
Arthur Coutinho <125709418+ArthurCoutinho15@users.noreply.github.com>
2023-09-01 21:58:39 -0300

    Add files via upload

---
+++ ativ3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+
+int val1=10, val2=20;
+int *resultado; 
+
+ soma(val1, val, *resultado)
+printf("Resultado: %d", resultado);
+printf(*\nendereco de memoria de Resultado:  );
+}    
+
+void soma(int val1, val2, int *r){
+	*r = val1 + val2; 
+}
